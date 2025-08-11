ff9ad7af74c023efad73013a6be555a6bb69a77b
binhduong160199 <if22b055@technikum-wien.at>
2025-08-11 21:51:31 +0200

    add more

--- DataWarehouse.Models/DTOs/UpdateUserDto.cs
+++ DataWarehouse.Models/DTOs/UpdateUserDto.cs
@@ -1,6 +1,13 @@
-namespace DataWarehouse.Models.DTOs;
-
-public class UpdateUserDto
+namespace DataWarehouse.Models.DTOs
 {
-    
+    public class UpdateUserDto
+    {
+        public string? FirstName { get; set; }
+        public string? LastName  { get; set; }
+        public string? Email     { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? NewPassword { get; set; }
+        public string? Role { get; set; } 
+    }
 }
